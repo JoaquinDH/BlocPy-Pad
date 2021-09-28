fe22ed3be48c344e33c306dfaac070d422e0ba35
JoaquinDH <JoaquinDahmer2020@gmail.com>
2021-09-28 18:58:09 -0300

    Inicio

---
+++ bloc.py
@@ -0,0 +1,55 @@
+
+from PyQt5.QtWidgets import *
+import sys
+
+f = open("texto.txt", "r")
+leido = f.readlines()
+    
+
+class VentanaPrincipal(QMainWindow):
+    def __init__(self,):
+        super().__init__()
+        ancho = 800
+        alto = 600
+        posicionIzquierda = 100
+        posicionDerecha = 100
+
+        self.setGeometry(posicionIzquierda, posicionDerecha, ancho, alto)
+        self.setWindowTitle("Mi Hijo")
+
+        contenedorVertical = QVBoxLayout()
+        boton = QPushButton()
+        
+
+        widgetPrincipal = QWidget()
+        widgetPrincipal.setLayout(contenedorVertical)
+
+        
+
+        self.setCentralWidget(widgetPrincipal)
+
+        contenedorHorizontal = QHBoxLayout()
+       
+        botonSaludar = QPushButton("Cargar")
+        contenedorHorizontal.addWidget(botonSaludar)
+
+        contenedorVertical.addLayout(contenedorHorizontal)
+
+
+        boton4 = QPushButton()
+        boton4.setText("Guardar")
+        contenedorHorizontal.addWidget(boton4)
+
+        texto = QTextEdit()
+        texto.setText(leido)
+        contenedorVertical.addWidget(texto)
+
+
+
+
+
+app = QApplication(sys.argv)
+
+ventana = VentanaPrincipal()
+ventana.show()
+app.exec()
